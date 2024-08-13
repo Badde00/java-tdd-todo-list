80575481809ef73ef3b926f08da41f76f9490be3
Iris Flystam <badde00@gmail.com>
2024-08-13 13:54:51 +0200

    GREEN: getFilteredTasks test works

--- src/main/java/com/booleanuk/core/Task.java
+++ src/main/java/com/booleanuk/core/Task.java
@@ -3,13 +3,23 @@ package com.booleanuk.core;
 public class Task {
     private String name;
     private String description;
+    private boolean completed;
 
     public Task(String name, String description) {
         this.name = name;
         this.description = description;
+        this.completed = false;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void complete() {
+        completed = true;
     }
 }
--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -16,4 +16,14 @@ public class TodoList {
     public void addTask(Task task) {
         tdl.add(task);
     }
+
+    public ArrayList<Task> getTasks(boolean filterCompleted) {
+        ArrayList<Task> filtered = new ArrayList<Task>();
+        for (Task t: tdl) {
+            if (t.isCompleted()) {
+                filtered.add(t);
+            }
+        }
+        return filtered;
+    }
 }
