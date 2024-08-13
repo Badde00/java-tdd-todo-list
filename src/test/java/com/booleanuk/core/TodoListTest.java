4df0a225db76239a60abd3abf6439df53a6b3f8b
Iris Flystam <badde00@gmail.com>
2024-08-13 13:24:00 +0200

    GREEN: fixed typo in test and made relevant funcitons

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -9,4 +9,13 @@ class TodoListTest {
         Task task = new Task("Sleep", "Get to sleep on time");
         Assertions.assertEquals("Sleep", task.getName());
     }
+
+    @Test
+    public void canTasksBeAdded() {
+        TodoList tdl = new TodoList();
+        Task task = new Task("a", "a");
+        Assertions.assertEquals(0, tdl.getTasks().size());
+        tdl.addTask(task);
+        Assertions.assertEquals(1, tdl.getTasks.size());
+    }
 }
