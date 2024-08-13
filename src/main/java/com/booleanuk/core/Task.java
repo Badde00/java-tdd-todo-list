8fd9965688d85da592c55f5241d59815c353ffd4
Iris Flystam <badde00@gmail.com>
2024-08-13 13:12:52 +0200

    GREEN: test passes

---
+++ src/main/java/com/booleanuk/core/Task.java
@@ -0,0 +1,15 @@
+package com.booleanuk.core;
+
+public class Task {
+    private String name;
+    private String description;
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
